3edc1ec9a111515b19a9404781bade3da2d70cf5
Muhammad Faris <deployments@muhdfaris.com>
2025-06-20 11:52:05 +0800

    feat: design exchange rate ui

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -8,4 +8,49 @@ import { Component } from '@angular/core';
 })
 export class DashboardComponent {
 
-}
+  summaryCards = [
+    {
+      title: 'Total Currencies',
+      value: '20',
+      icon: '/icons/globe-americas-solid.svg'
+    },
+    {
+      title: 'Positive Changes',
+      value: '5',
+      icon: '/icons/arrow-trending-up.svg'
+    },
+    {
+      title: 'Base Currency',
+      value: 'USD',
+      icon: '/icons/banknotes-solid.svg'
+    },
+    {
+      title: 'Last Updated',
+      value: '10 minutes ago',
+      icon: '/icons/clock-solid.svg'
+    }
+  ];
+  baseCurrencyOpts = [
+    { code: 'USD', name: 'US Dollar', symbol: '$' },
+    { code: 'EUR', name: 'Euro', symbol: '€' },
+    { code: 'GBP', name: 'British Pound', symbol: '£' },
+    { code: 'JPY', name: 'Japanese Yen', symbol: '¥' },
+    { code: 'AUD', name: 'Australian Dollar', symbol: 'A$' },
+    { code: 'CAD', name: 'Canadian Dollar', symbol: 'C$' },
+    { code: 'CHF', name: 'Swiss Franc', symbol: 'CHF' },
+    { code: 'CNY', name: 'Chinese Yuan', symbol: '¥' },
+    { code: 'SEK', name: 'Swedish Krona', symbol: 'kr' },
+    { code: 'NZD', name: 'New Zealand Dollar', symbol: 'NZ$' },
+    { code: 'MXN', name: 'Mexican Peso', symbol: '$' },
+    { code: 'SGD', name: 'Singapore Dollar', symbol: 'S$' },
+    { code: 'HKD', name: 'Hong Kong Dollar', symbol: 'HK$' },
+    { code: 'NOK', name: 'Norwegian Krone', symbol: 'kr' },
+    { code: 'TRY', name: 'Turkish Lira', symbol: '₺' },
+    { code: 'RUB', name: 'Russian Ruble', symbol: '₽' },
+    { code: 'INR', name: 'Indian Rupee', symbol: '₹' },
+    { code: 'BRL', name: 'Brazilian Real', symbol: 'R$' },
+    { code: 'ZAR', name: 'South African Rand', symbol: 'R' },
+    { code: 'KRW', name: 'South Korean Won', symbol: '₩' }
+  ];
+
+};
