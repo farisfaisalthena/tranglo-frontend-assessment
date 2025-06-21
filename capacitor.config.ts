c365b2c8e0bb0cd57ad3d9d281425ab107ae66aa
Muhammad Faris <deployments@muhdfaris.com>
2025-06-21 22:59:00 +0800

    chore: added capacitor package

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.tranglo.assessment',
+  appName: 'tranglo-frontend-assessment',
+  webDir: 'dist/tranglo-frontend-assessment/browser'
+};
+
+export default config;
