e2fe41931574b8f471b14069bdd0ec259eda5deb
Muhammad Faris <deployments@muhdfaris.com>
2025-06-21 23:11:51 +0800

    feat: added required dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
+
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+
+defineCustomElements(window);
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
